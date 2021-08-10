18a3f721a6c3d90facb696adbdbf9ed20fc30b00
Juan Blasco <juablave@gmail.com>
2021-08-10 21:53:49 +0200

    resolve ShoppingList.jsx linter and prettier issues

--- src/ShoppingList/ShoppingList.jsx
+++ src/ShoppingList/ShoppingList.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import './style/ShoppingList.css'
 
 function ShoppingList() {
-
-  return (
-    <div className="ShoppingList">
-      <p>This is working</p>
-    </div>
-  )
+    return (
+        <div className="ShoppingList">
+            <p>This is working</p>
+        </div>
+    )
 }
 
 export default ShoppingList
